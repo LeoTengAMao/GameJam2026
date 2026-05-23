48817ef09ff7aa0ab5feb8ca2fe93dbe7eaf9c24
MysteryStarfish <paulhsu1016@gmail.com>
2026-05-23 19:01:29 +0800

    feat: 新增 setting 頁面

--- Script/Menu/ChangeSceneManager.gd
+++ Script/Menu/ChangeSceneManager.gd
@@ -3,8 +3,11 @@ extends Node
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Tscn/MapManager.tscn")
 	
+func _on_setting_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Tscn/setting.tscn")
+
 func _on_staff_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Tscn/Staffs.tscn")
+	get_tree().change_scene_to_file("res://Tscn/Staff.tscn")
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit();
