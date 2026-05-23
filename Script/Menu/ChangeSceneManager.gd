fcce76428ec4d8dd512913afec0d0e25673a9011
MysteryStarfish <paulhsu1016@gmail.com>
2026-05-23 17:40:54 +0800

    feat: 新增 Menu 跟 Staff 場景
    - 目前 bg 為暫時

---
+++ Script/Menu/ChangeSceneManager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Tscn/MapManager.tscn")
+	
+func _on_staff_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Tscn/Staffs.tscn")
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit();
